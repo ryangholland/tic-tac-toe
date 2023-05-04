d1b134765ad07e60ebadfcf4826c0f686ef97e10
ryangholland <rghcode@gmail.com>
2023-05-03 22:18:51 -0400

    Dynamically render board and add basic grid styling

--- script.js
+++ script.js
@@ -23,16 +23,38 @@ const gameBoard = () => {
 
 const gameController = () => {
   let board = gameBoard();
-  let playerOne = player("Player One", "X");
-  let playerTwo = player("Player Two", "O");
+  let playerOne = Player("Player One", "X");
+  let playerTwo = Player("Player Two", "O");
 
+  const getBoard = () => board.getBoard();
 
-  return {};
+  return { getBoard };
 };
 
 const displayController = () => {
-  let game = gameController();
+  const boardContainer = document.getElementById("board-container");
 
+  let game = gameController();
+  let board = game.getBoard();
+
+  const clearBoard = () => {
+    while (boardContainer.firstChild) {
+      boardContainer.removeChild(boardContainer.firstChild);
+    }
+  };
+
+  const renderBoard = () => {
+    clearBoard();
+    board.forEach((cell) => {
+      let cellDiv = document.createElement("div");
+      cellDiv.textContent = cell.marker;
+      boardContainer.append(cellDiv);
+    });
+  };
+
+  renderBoard();
 
   return {};
 };
+
+displayController();
