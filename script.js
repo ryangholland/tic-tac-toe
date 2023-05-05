d6c5dd3776077a6c0605bc77082343d2071ca7ff
ryangholland <rghcode@gmail.com>
2023-05-04 23:34:57 -0400

    Use list of winning combos to check for winner

--- script.js
+++ script.js
@@ -39,10 +39,36 @@ const gameController = () => {
   const setMarker = (space) => {
     if (!board[space].marker) {
       board[space].marker = activePlayer.marker;
+      checkForWinner();
       changeActivePlayer();
-      console.log("Marker placed.");
-      console.log({ activePlayer });
     }
+  };
+
+  const checkForWinner = () => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+
+    winningCombos.forEach((combo) => {
+      let first = combo[0];
+      let second = combo[1];
+      let third = combo[2];
+
+      if (
+        board[first].marker &&
+        board[first].marker === board[second].marker &&
+        board[second].marker === board[third].marker
+      ) {
+        console.log("winna winna chicken dinna");
+      }
+    });
   };
 
   return { getBoard, getActivePlayer, setMarker };
